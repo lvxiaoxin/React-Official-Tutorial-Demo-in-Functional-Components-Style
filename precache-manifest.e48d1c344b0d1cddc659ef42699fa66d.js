c5704deb19ea244c8dedce50cc7a6ee5e6c0fd80
lvxiaoxin <lvxin0405@outlook.com>
2020-01-02 00:07:49 +0800

    Updates

---
+++ precache-manifest.e48d1c344b0d1cddc659ef42699fa66d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841578d1cffd5df6dbdbd4fb6e1eed27",
+    "url": "/react-official-demo/index.html"
+  },
+  {
+    "revision": "70f79a99486c11739c65",
+    "url": "/react-official-demo/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "72c523d6247dcef3e29c",
+    "url": "/react-official-demo/static/js/2.d4fe8c0f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-official-demo/static/js/2.d4fe8c0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70f79a99486c11739c65",
+    "url": "/react-official-demo/static/js/main.cb4ecee9.chunk.js"
+  },
+  {
+    "revision": "744bc88d2b02cf944519",
+    "url": "/react-official-demo/static/js/runtime-main.c39eb8cc.js"
+  }
+]);
